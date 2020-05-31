b180a4e1b3a7c662bed4fcfd384a4a67e14560e6
Callum <nailercallum@gmail.com>
2020-05-31 01:23:06 +0100

    Addition of CV to site.

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,6 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def cv(request):
+    return render(request, 'blog/cv.html', {})
